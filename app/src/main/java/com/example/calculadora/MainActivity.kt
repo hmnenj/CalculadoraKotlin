484fb6d5f05e84b6e54db180487395fad3d45b7b
unknown <Heloysa_23786@ETEC.FURLAN>
2024-06-11 12:49:12 -0300

    11/06

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.calculadora
 
+import android.widget.Button
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.calculadora.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+   //binding
+    private lateinit var binding: ActivityMainBinding
+
+    //variaveis
+    private var primeiraNumero = ""
+    private var numeroAtual = ""
+    private var operadorAtual = ""
+    private  var resultado = ""
+    @SuppressLint("SetText18")
+    //criacao de funcao android studio
+    override  fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //limitar tela
+
     }
 }
