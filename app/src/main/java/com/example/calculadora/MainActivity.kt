ee1df11c2499acaaae822ff5f701611e2a23e132
unknown <Heloysa_23786@ETEC.FURLAN>
2024-06-18 12:48:24 -0300

    18/06

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -1,19 +1,20 @@
 package com.example.calculadora
 
-import android.widget.Button
 import android.annotation.SuppressLint
-import android.app.Activity
-import android.view.WindowManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.view.WindowManager
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.children
 import com.example.calculadora.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
    //binding
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding : ActivityMainBinding
 
     //variaveis
-    private var primeiraNumero = ""
+    private var primeiroNumero = ""
     private var numeroAtual = ""
     private var operadorAtual = ""
     private  var resultado = ""
@@ -24,7 +25,31 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //limitar tela
+    //limitar tela
+    window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
 
+        //views
+        binding.apply {
+            //obter todos os botoes
+            layoutMain.children.filterIsInstance<Button>().forEach{ botao ->
+                //clique dos botoes
+                botao.setOnClickListener{
+                    //obter o texto do botao clicado
+                    val textoDoBotao = botao.text.toString()
+                    when{
+                        textoDoBotao.matches(Regex("[0-9]")) -> {
+                            if (operadorAtual.isEmpty()){
+                                primeiroNumero += textoDoBotao
+                                tvResult.text = primeiroNumero
+                            } else{
+                                numeroAtual += textoDoBotao
+                                tvResult.text = numeroAtual
+                            }
+                        }
+                        textoDoBotao.matches(Regex("[+\\-*/]")) ->
+                    }
+                }
+            }
+        }
     }
 }
